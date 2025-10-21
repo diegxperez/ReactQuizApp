32999cf2bb99c611b3943fa47aaee154a8c74755
Diego Perez <diegouceda0@gmail.com>
2025-10-20 23:03:29 -0500

    feat: Agregue componentes del Quiz y estilos
    - Add Components: ProgressBar, InitialCard
    - Actualice variables CSS para las pantallas
    - Organice la estructura del Layout

--- src/pages/homepage/HomePage.tsx
+++ src/pages/homepage/HomePage.tsx
@@ -1,17 +1,13 @@
 import s from './homepage.module.css'
-import react from '../../assets/react.svg'
+import { ProgressBar } from "../components/progressbar/ProgressBar"
+import { Questions } from '../components/question/Questions'
 
 export const HomePage = () => {
   return (
     <>
       <div className={s.card}>
-        <figure className={s.card__figure}>
-          <img src={react} alt="react logo" />
-        </figure>
-        <h1 className={s.card__title}>Quiz de React</h1>
-        <p className={s.card__description}>Prueba tus conocimientos sobre React</p>
-        <p className={s.card__description}>10 Preguntas</p>
-        <button className={s.card__button}>Comenzar Quiz</button>
+        <ProgressBar />
+        <Questions />
       </div>
     </>
   )
